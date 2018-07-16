9282769fa5a0dd1e281b569457bcae7f9061aa94
Jaouad Sarouti <jadsarouti@outlook.com>
2018-07-16 15:19:19 -0400

    second commit javas

--- jediname.js
+++ jediname.js
@@ -5,3 +5,21 @@ function jediName(firstName,lastName){
 }
 
 console.log(jediName('jedi','name'));
+
+
+function beyond(num){
+    let value = " ";
+    if(num === Infinity){
+        value = " Infinity ";
+    }else if(num > 0){
+        value = "bigger than 0";
+    }else if (num < 0 ){
+        value ="less than 0"
+    }else if (num === 0){
+        value = "equal to 0";
+    }
+    return value;
+}
+
+let myval = beyond(12);
+console.log(myval);
